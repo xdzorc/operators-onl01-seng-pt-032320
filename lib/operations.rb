6137844e3b8d4cf56874d038614629e5cd28946b
DAZUN XING <dazun.xing@gmail.com>
2020-03-31 14:01:34 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,19 @@
-def unsafe?(speed)
 
+
+def unsafe?(speed=0)
+  if speed<40||speed>60
+     "true"
+  else
+    "false"
+  
+  end
 end
 
 
 
 def not_safe?(speed)
-	
+	speed<40||speed>60?"true":"false"
 end
 	
 
-
+# unsafe?(50)
