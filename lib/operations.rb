1a5d3290a4855d2de17ddf76391901373f48f1df
DAZUN XING <dazun.xing@gmail.com>
2020-03-31 14:23:33 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,21 @@
-def unsafe?(speed)
 
+# require 'pry'
+def unsafe?(speed=0)
+  if speed<40||speed>60
+     true
+  else
+     false
+  
+  end
 end
 
 
 
 def not_safe?(speed)
-	
+	speed<40||speed>60? true:false
 end
 	
-
+learn submit
+unsafe?(95)
+# binding.pry
 
